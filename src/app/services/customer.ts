8b8cf2dc5ebc753cf734e29f61424eb729d3ca19
nurag-pradhan <pradhananurag2005@gmail.com>
2025-06-18 09:42:19 +0530

    New customers added

--- src/app/services/customer.ts
+++ src/app/services/customer.ts
@@ -7,7 +7,10 @@ export class CustomerService {
   private customers = [
     { id: 1, name: 'Rajesh Kumar', mobile: '9876543210', address: 'Bhubaneswar' },
     { id: 2, name: 'Sunita Sahu', mobile: '8887654321', address: 'Cuttack' },
-    { id: 3, name: 'Akash Das', mobile: '9090909090', address: 'Puri' }
+    { id: 3, name: 'Akash Das', mobile: '9090909090', address: 'Puri' },
+    { id: 4, name: 'Neha Mishra',mobile: '7878787878',address: 'Rourkela'},
+    { id: 5, name: 'Manas Ranjan',mobile: '7676767676',address: 'Balasore'}
+
   ];
 
   getCustomers() {
